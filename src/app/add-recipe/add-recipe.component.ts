75c485655f7939e761cab804361b52947c379bc9
SriRajendraT <rajendratm120@gmail.com>
2023-12-11 14:51:37 +0530

    common merge

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { DataApiService } from '../services/dataapi.service';
 import { RecipeDetails } from '../models/RecipeDetails-model';
 import { ApiResponse } from '../models/ApiResponse-model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Ingredient } from '../models/Ingredient-model';
 
 @Component({
   selector: 'app-add-recipe',
@@ -10,20 +11,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add-recipe.component.css'],
 })
 export class AddRecipeComponent implements OnInit {
+  recipe:RecipeDetails|null=null;
   recipeForm: FormGroup = new FormGroup({});
   constructor(private dataapi: DataApiService, private builder: FormBuilder) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.inItForm();
+  }
 
   inItForm() {
     this.recipeForm = this.builder.group({
-      ID: [0],
-
-      RECIPE_NAME: ['', [Validators.required]],
-      DESCRIPTION: ['', [Validators.required]],
+      id: [0],
+      recipe_name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
     });
   }
 
-  addRecipe(request: ApiResponse<RecipeDetails>) {
-    this.dataapi.addRecipe(request);
+  // addRecipe(request: ApiResponse<RecipeDetails>) {
+  //   this.dataapi.addRecipe(request);
+  // }
+
+  updateIngredients(data:any){
+console.log(data);
+
   }
 }
--- src/app/ingredientspartialviewe/ingredientspartialviewe.component.ts
+++ src/app/ingredientspartialviewe/ingredientspartialviewe.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Ingredient } from '../models/Ingredient-model';
+import { RecipeDetails } from '../models/RecipeDetails-model';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-ingredientspartialviewe',
   templateUrl: './ingredientspartialviewe.component.html',
   styleUrls: ['./ingredientspartialviewe.component.css']
 })
-export class IngredientspartialvieweComponent {
-ingridents:Ingredient[]=[];
+export class IngredientspartialvieweComponent implements OnInit {
+  constructor(private builder: FormBuilder) {
+    
+  }
+  ngOnInit(): void {
+  }
+  @Input() ingredints:Ingredient[]=[];
+  @Output() updatedIng=new EventEmitter<Ingredient[]|null>;
+
+  ingredientsForm:FormGroup=new FormGroup({});
+  inItForm(){
+    this.ingredientsForm=this.builder.group({
+
+    });
+  }
 
 }
